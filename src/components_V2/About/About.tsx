82937d843da3bedd09ed8c253608661d9a3c01aa
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2023-02-27 16:48:01 +0300

    feat: media 756px

--- src/components_V2/About/About.tsx
+++ src/components_V2/About/About.tsx
@@ -9,13 +9,6 @@ const AboutInner: FC = () => {
         Контакты
       </div>
       <div className="about__info">
-        <form className="about__form">
-          <input className="about__form__input" type="text" placeholder='Имя'/>
-          <input className="about__form__input" type="text" placeholder='Телефон'/>
-          <input className="about__form__input" type="text" placeholder='Почта'/>
-          <textarea className="about__form__input" placeholder='Сообщение'></textarea>
-          <input className="btn btn-secondary about__form__input_btn" type="submit" value="Отправить сообщение"/>
-        </form>
         <div className="about__adress">
           <div className="about__adress__item">
             <div className="about__adress__item__title">
@@ -62,6 +55,13 @@ const AboutInner: FC = () => {
             </div>
           </div>
         </div>
+        <form className="about__form">
+          <input className="about__form__input" type="text" placeholder='Имя'/>
+          <input className="about__form__input" type="text" placeholder='Телефон'/>
+          <input className="about__form__input" type="text" placeholder='Почта'/>
+          <textarea className="about__form__input" placeholder='Сообщение'></textarea>
+          <input className="btn btn-secondary about__form__input_btn" type="submit" value="Отправить сообщение"/>
+        </form>
       </div>
       <div className="about__map">
         {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2349.8818026866124!2d27.469040316037233!3d53.91607623958372!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dbc524915a4c9b%3A0xeb6537c79aefc108!2z0YPQuy4g0J_QtdGC0YDQsCDQk9C70LXQsdC60LggMTEsINCc0LjQvdGB0Lo!5e0!3m2!1sru!2sby!4v1677065410564!5m2!1sru!2sby" width="800" height="600"  loading="lazy" ></iframe> */}
