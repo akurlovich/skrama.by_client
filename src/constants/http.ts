6689e34686de343af400b916677d3cb44012740f
akurlovich <71250705+akurlovich@users.noreply.github.com>
2023-03-07 17:11:32 +0300

    fix: server skrama url

--- src/constants/http.ts
+++ src/constants/http.ts
@@ -1,5 +1,6 @@
 // export const SERVER_URL = 'http://localhost:4000/';
-export const SERVER_URL = 'http://185.179.83.227:4000/';
+// export const SERVER_URL = 'http://185.179.83.227:4000/';
+export const SERVER_URL = 'http://server.skrama.by/';
 // export const SERVER_URL = 'https://skrama-server.onrender.com/';
 export const API_URL = SERVER_URL + 'api/';
 
