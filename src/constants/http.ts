7f5d3cb3b8bca585dedad2ec060ea96661a56f98
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2023-03-17 11:17:04 +0300

    feat: 3

--- src/constants/http.ts
+++ src/constants/http.ts
@@ -1,6 +1,6 @@
 // export const SERVER_URL = 'http://localhost:4000/';
 // export const SERVER_URL = 'http://185.179.83.227:4000/';
-export const SERVER_URL = 'http://api.skrama24.by/';
+export const SERVER_URL = 'https://api.skrama24.by/';
 // export const SERVER_URL = 'https://skrama-server.onrender.com/';
 export const API_URL = SERVER_URL + 'api/';
 
