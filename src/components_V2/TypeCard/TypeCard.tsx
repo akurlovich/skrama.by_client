333ffe00a70cd891426bad192fcc37f8fc607daf
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2024-08-06 12:51:25 +0300

    feat: main block card layout build

--- src/components_V2/TypeCard/TypeCard.tsx
+++ src/components_V2/TypeCard/TypeCard.tsx
@@ -13,9 +13,15 @@ const TypeCardInner: FC<IProps> = ({title, text, linkTo, imgSrc}) => {
 	const navigate = useNavigate();
 
 	return (
-		<div className='typecard'>
+		<div
+			onClick={() => navigate(`/${linkTo}`)} 
+			className='typecard'
+		>
 			<div className="typecard__card">
 				<div className="typecard__card__image">
+					<div 
+						style={{backgroundImage: `url(${imgSrc})`}}
+						className="typecard__card__image_img"></div>
 					<img src={imgSrc} loading="lazy"/>
 					{/* <img src='images/main_img5.jpg' loading="lazy"/> */}
 				</div>
@@ -23,12 +29,12 @@ const TypeCardInner: FC<IProps> = ({title, text, linkTo, imgSrc}) => {
 					<h2>{title}</h2>
 					<h3>{text}</h3>
 				</div>
-				<button 
+				{/* <button 
 					className="typecard__card__button"
 					onClick={() => navigate(`/${linkTo}`)}
 				>
 					Выбрать
-				</button>
+				</button> */}
 
 			</div>
 		</div>
